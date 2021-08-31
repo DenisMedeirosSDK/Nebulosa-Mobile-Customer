c8de772c7bcfe0b22f122a0a7c7d88c1ad068c19
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-08-31 10:34:55 -0300

    test: create route test for using screen signin

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,12 +7,14 @@ import { ServicesDetails } from '../screens/ServicesDetails';
 import { AppointmentDetails } from '../screens/AppointmentDetails';
 
 import { TabRoutes } from './tab.routes';
+import { SignIn } from '../screens/SignIn';
 
 const Stack = createStackNavigator();
 
 export function StackRoutes() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="TabRoutes" component={TabRoutes} />
       <Stack.Screen name="ListServices" component={ListServices} />
       <Stack.Screen name="ServicesDetails" component={ServicesDetails} />
