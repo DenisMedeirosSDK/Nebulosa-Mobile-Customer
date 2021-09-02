717e95fc537c45a64c7166bacc002510d6ccb9eb
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-09-02 17:23:19 -0300

    feat: persist data in database and create signOut

--- src/hook/auth.tsx
+++ src/hook/auth.tsx
@@ -1,16 +1,22 @@
-import React, { createContext, useState, useContext, ReactNode } from 'react';
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from 'react';
+
+import { database } from '../database';
+import { User } from '../database/model/User';
 import { api } from '../services/api';
 
-interface User {
+interface IUser {
   id: string;
+  userId: string;
   name: string;
   email: string;
-}
-
-interface AuthState {
   token: string;
   refreshToken: string;
-  user: User;
 }
 
 interface SignInCredentials {
@@ -19,8 +25,9 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: User;
+  user: IUser;
   signIn: (credentials: SignInCredentials) => Promise<void>;
+  signOut: () => Promise<void>;
 }
 
 interface AuthProviderProps {
@@ -30,27 +37,72 @@ interface AuthProviderProps {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const [data, setData] = useState<AuthState>({} as AuthState);
+  const [data, setData] = useState<IUser>({} as IUser);
 
   async function signIn({ email, password }: SignInCredentials) {
-    const respone = await api.post('/sessions', {
-      email,
-      password,
-    });
+    try {
+      const respone = await api.post('/sessions', {
+        email,
+        password,
+      });
 
-    const { token, refreshToken, user } = respone.data;
-    console.log(respone.data);
+      const { token, refreshToken, user } = respone.data;
 
-    api.defaults.headers.authorization = `Bearer ${refreshToken}`;
+      api.defaults.headers.authorization = `Bearer ${refreshToken}`;
 
-    setData({ token, refreshToken, user });
+      const userCollection = database.get<User>('users');
+
+      await database.write(async () => {
+        await userCollection.create(newUser => {
+          (newUser.userId = user.id),
+            (newUser.name = user.name),
+            (newUser.email = user.email),
+            (newUser.token = token),
+            (newUser.refreshToken = refreshToken);
+        });
+      });
+
+      setData({ token, refreshToken, ...user });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
+
+  async function signOut() {
+    try {
+      const userCollection = database.get<User>('users');
+      await database.write(async () => {
+        const userSelected = await userCollection.find(data.id);
+        await userSelected.destroyPermanently();
+      });
+
+      setData({} as IUser);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  useEffect(() => {
+    async function loadUserStorage() {
+      const userCollection = database.get<User>('users');
+      const response = await userCollection.query().fetch();
+
+      if (response.length > 0) {
+        const userData = response[0]._raw as unknown as User;
+        api.defaults.headers.authorization = `Bearer ${userData.refreshToken}`;
+        console.log(userData.refreshToken);
+        setData(userData);
+      }
+    }
+    loadUserStorage();
+  });
 
   return (
     <AuthContext.Provider
       value={{
-        user: data.user,
+        user: data,
         signIn,
+        signOut,
       }}
     >
       {children}
