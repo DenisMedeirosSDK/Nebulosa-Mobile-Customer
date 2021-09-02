810fca3fc4a21631079c406982cadc64e1bb8312
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-09-02 08:17:08 -0300

    feat: create auth route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { StackRoutes } from './stack.routes';
+import { AuthRoutes } from './auth.routes';
+
+import { useAuth } from '../hook/auth';
 
 export function Routes() {
+  const { user } = useAuth();
   return (
     <NavigationContainer>
-      <StackRoutes />
+      {user ? <StackRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,16 +7,12 @@ import { ServicesDetails } from '../screens/ServicesDetails';
 import { AppointmentDetails } from '../screens/AppointmentDetails';
 
 import { TabRoutes } from './tab.routes';
-import { SignIn } from '../screens/SignIn';
-import { SignUp } from '../screens/SignUp';
 
 const Stack = createStackNavigator();
 
 export function StackRoutes() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="SignUp" component={SignUp} />
       <Stack.Screen name="TabRoutes" component={TabRoutes} />
       <Stack.Screen name="ListServices" component={ListServices} />
       <Stack.Screen name="ServicesDetails" component={ServicesDetails} />
